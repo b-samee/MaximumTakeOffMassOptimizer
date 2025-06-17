93909911690d8b3753c7e825e4957eaa35b110b3
b-samee <b_samee@live.concordia.ca>
2025-06-17 17:06:43 -0400

    Moved logging

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ import logging
 
 def main() -> None:
     logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
-    logger = logging.getLogger()
     
     config = ConfigurationLoader()
 
     for run_configuration in config.run_configurations:
-        logger.info(f'EXECUTING RUN {run_configuration["index"]}/{len(config.run_configurations)}')
         model = DynamicThrustModel(run_configuration, config.configuration_name)
         model.run()
 
