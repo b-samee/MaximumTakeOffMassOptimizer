c3f3db75326056e0627b8455d807c4ec3ad48e0c
b-samee <b_samee@live.concordia.ca>
2025-06-17 16:15:28 -0400

    Added Pairing

--- components/utils/type_aliases.py
+++ components/utils/type_aliases.py
@@ -2,4 +2,6 @@ import typing
 import pathlib
 import numpy
 
-RunConfiguration: typing.TypeAlias = dict[str, numpy.float64 | dict[str, numpy.float64] | list[dict[str, pathlib.Path]]]
+Pairing: typing.TypeAlias = dict[str, pathlib.Path]
+
+RunConfiguration: typing.TypeAlias = dict[str, numpy.float64 | dict[str, numpy.float64] | list[Pairing]]
