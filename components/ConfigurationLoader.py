c264fe0cb0ce545764865ce2646d3d251f8225eb
b-samee <b_samee@live.concordia.ca>
2025-06-17 16:09:48 -0400

    Updated for readability

--- components/ConfigurationLoader.py
+++ components/ConfigurationLoader.py
@@ -4,98 +4,109 @@ import numpy
 import json
 
 from components.utils.type_aliases import RunConfiguration
+import components.utils.config_keys as CONFIG
 
 class ConfigurationLoader(argparse.ArgumentParser):
     run_configurations: list[RunConfiguration]
     
     def __init__(self) -> None:
+        json_data = self.load_config()
+        self.validate_configuration(json_data)
+    
+
+
+    def load_config(self) -> list[RunConfiguration]:
         super().__init__(add_help=False)
         self.add_argument('config', type=str, help="Path to the input configuration json file")
         args = self.parse_args()
         
         json_path = pathlib.Path(args.config)
         if not json_path.exists():
-            raise FileNotFoundError(f'ERROR: configuration file does not exist at path "{json_path}"')
+            raise FileNotFoundError(f'configuration file does not exist at path "{json_path}"')
         
         self.run_configurations = list()
         with open(json_path, 'r') as json_file:
-            json_data = json.load(json_file)
-        
-        CONFIGURATION_KEYS = {'current_limit', 'setpoint', 'pairings'}
-        
-        SETPOINT_KEYS = {
-            'velocity',
-            'voltage',
-            'dbeta',
-            'current',
-            'torque',
-            'thrust',
-            'pele',
-            'rpm',
-        }
+            return json.load(json_file)
 
-        PAIRING_KEYS = {'propeller', 'motor'}
-        
-        error_prefix = 'ERROR: invalid configuration file format'
-        
+
+
+    def validate_configuration(self, json_data: list[RunConfiguration]) -> None:
         if type(json_data) != list:
-            raise TypeError(f'{error_prefix}, must begin with a list')
+            raise TypeError('configuration file must begin with a list')
         
         for run_configuration in json_data:
-            if type(run_configuration) != dict:
-                raise TypeError(f'{error_prefix}, root list can only contain objects')
+            self.validate_run(self, run_configuration)
+    
+
+
+    def validate_run(self, run_configuration: RunConfiguration) -> None:
+        if type(run_configuration) != dict:
+            raise TypeError(f'configuration file root list can only contain objects')
+        
+        RUN_CONFIGURATION_KEYS = set(run_configuration.keys())
+        
+        MISSING_KEYS = CONFIG.ALL_CONFIGURATION_KEYS - RUN_CONFIGURATION_KEYS
+        if len(MISSING_KEYS) != 0:
+            raise KeyError(f'configuration file missing configuration keys {MISSING_KEYS}')
+        UNKNOWN_KEYS = RUN_CONFIGURATION_KEYS - CONFIG.ALL_CONFIGURATION_KEYS
+        if len(UNKNOWN_KEYS) != 0:
+            raise KeyError(f'configuration file unknown configuration keys {UNKNOWN_KEYS}')
+        
+        self.validate_current_limit(run_configuration)
+        self.validate_setpoint(run_configuration)
+        self.validate_pairings(run_configuration)
+
+        self.run_configurations.append(run_configuration)
+    
+
+
+    def validate_current_limit(self, run_configuration: RunConfiguration) -> None:
+        if type(run_configuration['current_limit']) not in {int, float}:
+            raise ValueError(f'configuration file "current_limit" must point to a number')
+        run_configuration['current_limit'] = numpy.float64(run_configuration['current_limit'])
+    
+
+
+    def validate_setpoint(self, run_configuration: RunConfiguration) -> None:
+        if type(run_configuration['setpoint']) != dict:
+            raise ValueError(f'configuration file "setpoint" must point to an object')
+
+        RUN_SETPOINT_KEYS = set(run_configuration['setpoint'].keys())
+        
+        MISSING_KEYS = CONFIG.ALL_SETPOINT_KEYS - RUN_SETPOINT_KEYS
+        for key in MISSING_KEYS:
+            run_configuration['setpoint'][key] = 0
+        UNKNOWN_KEYS = RUN_SETPOINT_KEYS - CONFIG.ALL_SETPOINT_KEYS
+        if len(UNKNOWN_KEYS) != 0:
+            raise KeyError(f'configuration file unknown setpoint keys {UNKNOWN_KEYS}')
+        
+        for key, value in run_configuration['setpoint'].items():
+            if type(value) not in {int, float}:
+                raise ValueError(f'configuration file setpoint "{key}" must point to a number')
+            run_configuration['setpoint'][key] = numpy.float64(run_configuration['setpoint'][key])
+    
+
+
+    def validate_pairings(self, run_configuration: RunConfiguration) -> None:
+        if type(run_configuration['pairings']) != list:
+            raise ValueError(f'configuration file "pairings" must point to a list')
+
+        for pairing in run_configuration['pairings']:
+            if type(pairing) != dict:
+                raise TypeError(f'configuration file pairing must be an object')
+
+            RUN_PAIRING_KEYS = set(pairing.keys())
             
-            RUN_CONFIGURATION_KEYS = set(run_configuration.keys())
-            
-            MISSING_KEYS = CONFIGURATION_KEYS - RUN_CONFIGURATION_KEYS
+            MISSING_KEYS = CONFIG.ALL_PAIRING_KEYS - RUN_PAIRING_KEYS
             if len(MISSING_KEYS) != 0:
-                raise KeyError(f'{error_prefix}, missing configuration keys {MISSING_KEYS}')
-            UNKNOWN_KEYS = RUN_CONFIGURATION_KEYS - CONFIGURATION_KEYS
+                raise KeyError(f'configuration file missing pairing keys {MISSING_KEYS}')
+            UNKNOWN_KEYS = RUN_PAIRING_KEYS - CONFIG.ALL_PAIRING_KEYS
             if len(UNKNOWN_KEYS) != 0:
-                raise KeyError(f'{error_prefix}, unknown configuration keys {UNKNOWN_KEYS}')
-            
-            if type(run_configuration['current_limit']) not in {int, float}:
-                raise ValueError(f'{error_prefix}, "current_limit" must point to a number')
-            run_configuration['current_limit'] = numpy.float64(run_configuration['current_limit'])
-            
-            if type(run_configuration['setpoint']) != dict:
-                raise ValueError(f'{error_prefix}, "setpoint" must point to an object')
+                raise KeyError(f'configuration file unknown pairing keys {UNKNOWN_KEYS}')
 
-            RUN_SETPOINT_KEYS = set(run_configuration['setpoint'].keys())
-            
-            MISSING_KEYS = SETPOINT_KEYS - RUN_SETPOINT_KEYS
-            for key in MISSING_KEYS:
-                run_configuration['setpoint'][key] = 0
-            UNKNOWN_KEYS = RUN_SETPOINT_KEYS - SETPOINT_KEYS
-            if len(UNKNOWN_KEYS) != 0:
-                raise KeyError(f'{error_prefix}, unknown setpoint keys {UNKNOWN_KEYS}')
-            
-            for key, value in run_configuration['setpoint'].items():
-                if type(value) not in {int, float}:
-                    raise ValueError(f'{error_prefix}, setpoint "{key}" must point to a number')
-                run_configuration['setpoint'][key] = numpy.float64(run_configuration['setpoint'][key])
-            
-            if type(run_configuration['pairings']) != list:
-                raise ValueError(f'{error_prefix}, "pairings" must point to a list')
-
-            for pairing in run_configuration['pairings']:
-                if type(pairing) != dict:
-                    raise TypeError(f'{error_prefix}, pairing must be an object')
-
-                RUN_PAIRING_KEYS = set(pairing.keys())
-                
-                MISSING_KEYS = PAIRING_KEYS - RUN_PAIRING_KEYS
-                if len(MISSING_KEYS) != 0:
-                    raise KeyError(f'{error_prefix}, missing pairing keys {MISSING_KEYS}')
-                UNKNOWN_KEYS = RUN_PAIRING_KEYS - PAIRING_KEYS
-                if len(UNKNOWN_KEYS) != 0:
-                    raise KeyError(f'{error_prefix}, unknown pairing keys {UNKNOWN_KEYS}')
-
-                for key, value in pairing.items():
-                    if type(value) != str:
-                        raise ValueError(f'{error_prefix}, pairing "{key}" must point to a string')
-                    pairing[key] = pathlib.Path(value)
-                    if not pairing[key].exists():
-                        raise FileNotFoundError(f'{error_prefix}, path to "{pairing[key]}" does not exist')
-            
-            self.run_configurations.append(run_configuration)
+            for key, value in pairing.items():
+                if type(value) != str:
+                    raise ValueError(f'configuration file pairing "{key}" must point to a string')
+                pairing[key] = pathlib.Path(value)
+                if not pairing[key].exists():
+                    raise FileNotFoundError(f'configuration file path to "{pairing[key]}" does not exist')
---
+++ components/utils/config_keys.py
@@ -0,0 +1,3 @@
+ALL_CONFIGURATION_KEYS = {'current_limit', 'setpoint', 'pairings'}
+ALL_SETPOINT_KEYS = {'velocity', 'voltage', 'dbeta', 'current', 'torque', 'thrust', 'pele', 'rpm'}
+ALL_PAIRING_KEYS = {'propeller', 'motor'}
