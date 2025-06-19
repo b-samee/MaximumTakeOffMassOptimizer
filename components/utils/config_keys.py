2c071bd1af64c39b2601fa4eaa7d9d6a865e6f99
b-samee <b_samee@live.concordia.ca>
2025-06-18 23:10:02 -0400

    Replaced config keys with config structure

--- components/utils/config_keys.py
+++
@@ -1,3 +0,0 @@
-ALL_CONFIGURATION_KEYS = {'propeller_file', 'motor_file', 'timestep_resolution', 'mass_range', 'cutoff_displacement', 'setpoint_parameters', 'drag_force'}
-ALL_SETPOINTPARAMETERS_KEYS = {'velocity', 'voltage', 'dbeta', 'current', 'torque', 'thrust', 'pele', 'rpm'}
-ALL_DRAGFORCE_KEYS = {'fluid_density', 'true_airspeed', 'drag_coefficient', 'reference_area'}
---
+++ components/utils/config_structure.py
@@ -0,0 +1,46 @@
+EXPECTED_CONFIGURATION_STRUCTURE = {
+    'propeller_file': str,
+    'motor_file': str,
+    'timestep_resolution': (float, int),
+    'mass_range': [(float, int), (float, int)],
+    'cutoff_displacement': [(float, int), (float, int)],
+    'setpoint_parameters': {
+        'velocity': (None, float, int),
+        'voltage': (float, int),
+        'dbeta': (None, float, int),
+        'current': (None, float, int),
+        'torque': (None, float, int),
+        'thrust': (None, float, int),
+        'pele': (None, float, int),
+        'rpm': (None, float, int)
+    },
+    'drag_force': {
+        'fluid_density': (float, int),
+        'true_airspeed': (None, float, int),
+        'drag_coefficient': (float, int),
+        'reference_area': (float, int)
+    }
+}
+
+def get_config_structure(json: object) -> object:
+    def get_json_structure(json: object, origin_keys: list[str]) -> object:
+        if isinstance(json, dict):
+            result = {key: get_json_structure(value, [key]) for key, value in json.items()}
+        elif isinstance(json, list):
+            result = list()
+            for value in json: result.append(get_json_structure(value, origin_keys))
+        elif json is None or isinstance(json, int) or isinstance(json, float):
+            result = EXPECTED_CONFIGURATION_STRUCTURE
+            try:
+                for key in origin_keys:
+                    result = result[key]
+                if not (json is None and None in result or type(json) in result):
+                    result = type(json)
+            except Exception as e:
+                result = type(json)
+        else:
+            result = type(json)
+        
+        return result
+    
+    json = get_json_structure(json)
