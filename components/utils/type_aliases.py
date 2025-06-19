cfacd2bfc23fe8e6340f21e3db2a4b89e5d5c550
b-samee <b_samee@live.concordia.ca>
2025-06-19 00:27:02 -0400

    Removed outdated type aliases

--- components/utils/type_aliases.py
+++
@@ -1,7 +0,0 @@
-import typing
-import pathlib
-import numpy
-
-Pairing: typing.TypeAlias = dict[str, pathlib.Path]
-
-RunConfiguration: typing.TypeAlias = dict[str, numpy.float64 | dict[str, numpy.float64] | list[Pairing]]
