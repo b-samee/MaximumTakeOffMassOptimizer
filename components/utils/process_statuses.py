593db7bfcc3ad761e0cbfda514e23af500bab0cf
b-samee <b_samee@live.concordia.ca>
2025-06-19 14:25:01 -0400

    Unnecessary

--- components/utils/process_statuses.py
+++ components/utils/process_statuses.py
@@ -14,25 +14,3 @@ class ProcessStatus(enum.Enum):
     @classmethod
     def get(cls, index: int):
         return list(cls)[index]
-    
-    def __repr__(self) -> str:
-        match self:
-            case ProcessStatus.SLEEPING:
-                return f'SLEEPING'
-            case ProcessStatus.STARTING:
-                return f'STARTING'
-            case ProcessStatus.EXECUTING_QPROP:
-                return f'EXECUTING_QPROP'
-            case ProcessStatus.EXTRACTING_DATA:
-                return f'EXTRACTING_DATA'
-            case ProcessStatus.CALCULATING:
-                return f'CALCULATING'
-            case ProcessStatus.UPDATING_COUNTERS:
-                return f'UPDATING_COUNTERS'
-            case ProcessStatus.CHECKING_CONDITION:
-                return f'CHECKING_CONDITION'
-            case ProcessStatus.REJECTED:
-                return f'REJECTED'
-            case ProcessStatus.ACCEPTED:
-                return f'ACCEPTED'
-        raise ValueError(f'Missing ProcessStatus match case for [{self}]')
